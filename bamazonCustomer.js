9eda091a24b824ae6ff3712d8e3ac2658289c5e0
iamDio <claudio.villani00@gmail.com>
2017-07-02 09:19:08 -0400

    App now checks if user entered item id is in the database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -56,20 +56,25 @@ const inquiries = function() {
                 //res is the data from the databases
 
                 res.forEach((res) => {
+                //shows order information
+
                     if (res.item_id == answer.id && answer.quantity <= res.stock_quantity) {
                         console.log(`product id: ${answer.id}`); 
                         console.log(`ordered product: ${res.product_name}`);
                         console.log(`units ordered ${answer.quantity}`);
                         console.log('order placed');
-                        
-                //updates mysql database to reflect user order
+                //updates mysql database to reflect user order and shows total cost of purchase order
                         let updatedStock = res.stock_quantity-answer.quantity;
+                        let total = answer.quantity * res.price;
                         connection.query('UPDATE products SET ? WHERE ?',
                             [{stock_quantity: updatedStock},{item_id: answer.id}]);
                         console.log(`remaining stock: ${updatedStock}`)
-                        console.log();
+                        console.log(`Total Cost: $${total}`);
 
-                    }else if (res.item_id == answer.id && answer.quantity > res.stock_quantity) {
+                    }else if(answer.id != res.item_id){
+                        console.log('Product not found. Please select a different product');
+                    }
+                    else if (res.item_id == answer.id && answer.quantity > res.stock_quantity) {
                     	console.log('not enough in stock')
                     }
                 })
